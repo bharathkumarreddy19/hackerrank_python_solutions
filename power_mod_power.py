6598f1b3c762365f956596690f814c4f3b80c008
bharathkumarreddy19 <64121431+bharathkumarreddy19@users.noreply.github.com>
2020-05-04 13:46:24 +0530

    Create power_mod_power.py

---
+++ power_mod_power.py
@@ -0,0 +1,10 @@
+import math
+a = input()
+b = input()
+m = input()
+
+print(int(math.pow(int(a),int(b))))
+if int(m) > 0:
+    print(pow(int(a),int(b),int(m)))
+else:
+    print("Please enter a positive value of
