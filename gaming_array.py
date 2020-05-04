45906fcb53482bedbf67bdf6276cb3e0cb15d8de
bharathkumarreddy19 <64121431+bharathkumarreddy19@users.noreply.github.com>
2020-05-04 13:25:57 +0530

    Create gaming_array.py

---
+++ gaming_array.py
@@ -0,0 +1,40 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gamingArray' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def gamingArray(arr):
+    # Write your code here
+    max=arr[0]
+    changes=1
+    for element in arr:
+        if element>max:
+            max=element
+            changes=changes+1
+    if changes%2==1:
+        return 'BOB'
+    else:
+        return 'ANDY'
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    g = int(input().strip())
+
+    for g_itr in range(g):
+        arr_count = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = gamingArray(arr)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
